476e98b9d675fe46627b779fdabff9261a89d9f8
Lego79 <kentcandoit89@gmail.com>
2022-10-17 23:16:42 +0900

    average

--- src/main/java/First/Average.java
+++ src/main/java/First/Average.java
@@ -1,2 +1,19 @@
-package First;public class Average {
-}
+package First;
+
+public class Average {
+    public static void main(String[] args) {
+        int[] numbers = new int[]{90, 85, 75, 80, 100, 60, 85};
+            double answer = 0;
+            double result = 0;
+            for (int i = 0; i < numbers.length; i++) {
+                result += numbers[i];
+                answer = result / numbers.length;
+            }
+
+            System.out.println("answer = " + answer);
+
+        }
+    }
+
+
+
