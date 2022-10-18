d841565377e86459fc41f00bb83e0a34822d834b
Lego79 <kentcandoit89@gmail.com>
2022-10-18 22:31:50 +0900

    10/18 dailyCoding

---
+++ src/main/java/second/CountLetter.java
@@ -0,0 +1,14 @@
+package second;
+
+public class CountLetter {
+    class Solution {
+        public int solution(String message) {
+            int answer = 0;
+
+            answer = message.length() * 2;
+
+
+            return answer;
+        }
+    }
+}
